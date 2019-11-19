20b2cab065adaaced09e30fd6954bb7ad7ee529e
Vitalik-D <vitalikdyaduchko@gmail.com>
2019-11-19 12:49:29 +0200

    Updates

---
+++ precache-manifest.5124538196c1823fb78ef7d3b0f4fb1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db3039e5d0b3492d7b29bd23f122313",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "eccd7e383e626ab46b01",
+    "url": "/Pokedex/static/css/main.34b166a5.chunk.css"
+  },
+  {
+    "revision": "6d05392a5e10ea4b631d",
+    "url": "/Pokedex/static/js/2.f0c84659.chunk.js"
+  },
+  {
+    "revision": "eccd7e383e626ab46b01",
+    "url": "/Pokedex/static/js/main.5a2ad3de.chunk.js"
+  },
+  {
+    "revision": "34dcb7c62138091398c4",
+    "url": "/Pokedex/static/js/runtime-main.ba04c60a.js"
+  },
+  {
+    "revision": "3d751cc6bdf6cad3c61e6292f92f009d",
+    "url": "/Pokedex/static/media/pngfind.com-ash-greninja-png-2124784.3d751cc6.png"
+  },
+  {
+    "revision": "9586f4ea23253f90d3118746356592c3",
+    "url": "/Pokedex/static/media/pokeball-icon-transparent-9.9586f4ea.jpg"
+  }
+]);
